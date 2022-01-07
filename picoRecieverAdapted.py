db2c71c750afeb9145f1d648a5857ab0d9e7e29f
Sergey Grigorovich <sergey.grigorovich@promwad.com>
2022-01-07 20:25:43 +0300

    feat: add strip wait function

--- picoRecieverAdapted.py
+++ picoRecieverAdapted.py
@@ -18,6 +18,15 @@ SERIAL_BAUDRATE = 115200
 colorsBuffer1 = ([0,0,0] * LED_COUNT)
 colorsBuffer2 = ([0,0,0] * LED_COUNT)
 
+statusState1 = 0
+statusState2 = 0
+
+incomingStatus1 = 0
+incomingStatus2 = 0
+
+changeFlag1 = 0
+changeFlag2 = 0
+
 pixels = Neopixel(OVERALL_LED_COUNT, 0, 22, "RGB")
 pixels.brightness(30)
 
@@ -40,13 +49,15 @@ def setNeopixelData(colorsArray, strip, pixelShift):
     strip.show()
         #strip.setPixelColor(i + pixelShift, colorsArray[i])   
 
-def doSerialRead (serialInstance, inputBuffer, colorsBuffer, stripShift):
+def doSerialRead (serialInstance, inputBuffer, incomingStatus, colorsBuffer, stripShift):
     if(serialInstance.any()):
+        incomingStatus = 1
         # Read data out of the buffer until a carraige return / new line is found
         readedByte = serialInstance.read(1)
         # print(readedByte)
         inputBuffer += readedByte
-        
+        if (len(inputBuffer) > 255):
+            inputBuffer = bytearray(b'')
         if (readedByte == b'\n' and inputBuffer[len(inputBuffer) - 2] == 13 ):
             if (len(inputBuffer) >= LED_COUNT*3):
                 #print ("".join("\\x%02x" % i for i in serialString))
@@ -54,12 +65,28 @@ def doSerialRead (serialInstance, inputBuffer, colorsBuffer, stripShift):
                 setNeopixelData(colorsBuffer, pixels, stripShift)
             inputBuffer = bytearray(b'')
 
+def waitIncomingData (incomingStatus, statusState, changeFlag, strip, stripShift):
+    if (incomingStatus == 0):
+        if (time.ticks_ms() % 500 == 0 and changeFlag == 0):
+            if (statusState == 0):
+                strip.set_pixel(stripShift + LED_COUNT - 1, [0,128,0])
+                statusState = 1
+                
+            else:
+                statusState = 0
+                strip.set_pixel(stripShift + LED_COUNT - 1, [0,0,0])
+            changeFlag = 1
+        else: 
+            changeFlag = 0
+
 while(1):
  
     # Wait until there is data waiting in the serial buffer
+    waitIncomingData(incomingStatus1, statusState1, changeFlag1, pixels, FIRST_STRIP_SHIFT)
+    waitIncomingData(incomingStatus2, statusState2, changeFlag2, pixels, SECOND_STRIP_SHIFT)
 
-    doSerialRead(serial1, serial1String, colorsBuffer1, FIRST_STRIP_SHIFT)
-    doSerialRead(serial2, serial2String, colorsBuffer2, SECOND_STRIP_SHIFT)
+    doSerialRead(serial1, serial1String, incomingStatus1, colorsBuffer1, FIRST_STRIP_SHIFT)
+    doSerialRead(serial2, serial2String, incomingStatus2, colorsBuffer2, SECOND_STRIP_SHIFT)
     
 
 
