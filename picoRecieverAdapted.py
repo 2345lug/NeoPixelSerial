bc868fbe6d5cbcd4a3d4d0a5ee348b68b1ca7a6c
Sergey Grigorovich <sergey.grigorovich@promwad.com>
2022-01-09 11:31:54 +0300

    feat: change idle status code

--- picoRecieverAdapted.py
+++ picoRecieverAdapted.py
@@ -9,7 +9,7 @@ LED_COUNT = 8 # Number of LED pixels.
 ##LED_PIN = 18 # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 FIRST_STRIP_SHIFT = 0
 SECOND_STRIP_SHIFT = 8
-
+STRIPS_COUNT = 2
 ##Serial port configuration##
 
 SERIAL_BAUDRATE = 115200
@@ -18,7 +18,7 @@ SERIAL_BAUDRATE = 115200
 colorsBuffer1 = ([0,0,0] * LED_COUNT)
 colorsBuffer2 = ([0,0,0] * LED_COUNT)
 
-pixels = Neopixel(LED_COUNT, 0, 22, "RGB")
+pixels = Neopixel(LED_COUNT * STRIPS_COUNT, 0, 22, "RGB")
 pixels.brightness(30)
 
 serialString1 = bytearray(b'')                           # Used to hold data coming over UART
@@ -27,6 +27,8 @@ serialString2 = bytearray(b'')
 serial1 = machine.UART(0, SERIAL_BAUDRATE)
 serial2 = machine.UART(1, SERIAL_BAUDRATE)
 
+serial1DataStatus = 0
+serial2DataStatus = 0
 
 def bytesToInt(byteValues, intValue):
     intValue = int.from_bytes(byteValues, "big")
@@ -46,10 +48,25 @@ while(1):
  
     # Wait until there is data waiting in the serial buffer
 
+    if (time.ticks_ms() % 500):
+        if (serial1DataStatus == 0):
+            pixels.set_pixel(LED_COUNT - 1, [0, 0, 255])
+        if (serial2DataStatus == 0):
+            pixels.set_pixel(LED_COUNT * 2 - 1, [0, 0, 255])
+
+    if (time.ticks_ms() % 1000):
+        if (serial1DataStatus == 0):
+            pixels.set_pixel(LED_COUNT - 1, [0, 0, 0])
+        if (serial2DataStatus == 0):
+            pixels.set_pixel(LED_COUNT * 2 - 1, [0, 0, 0])
+    
+    if (serial1DataStatus == 0 or serial2DataStatus == 0):
+        pixels.show()
+
     if(serial1.any()):
         readedByte1 = serial1.read(1)
         serialString1 += readedByte1
-        
+        serial1DataStatus = 1
         if (readedByte1 == b'\n' and serialString1[len(serialString1) - 2] == 13 ):
             if (len(serialString1) >= LED_COUNT*3):
                 #print ("".join("\\x%02x" % i for i in serialString1))
@@ -60,12 +77,16 @@ while(1):
     if(serial2.any()):
         readedByte2 = serial2.read(1)
         serialString2 += readedByte2
-        
+        serial2DataStatus = 1
         if (readedByte2 == b'\n' and serialString2[len(serialString2) - 2] == 13 ):
             if (len(serialString2) >= LED_COUNT*3):
                 #print ("".join("\\x%02x" % i for i in serialString2))
                 inputStringParse(serialString2, colorsBuffer2)
                 setNeopixelData(colorsBuffer2, pixels, SECOND_STRIP_SHIFT)
             serialString2 = bytearray(b'')
+
+
+
+
 
      
